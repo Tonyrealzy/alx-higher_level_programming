358aee59a28d8859c547b83b1a7729482e765dff
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-19 03:27:11 +0100

    Lists, Tuples tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,7 @@ def element_at(my_list, idx):
 
     if idx < 0:
         return None
-    if idx > len(my_list) or idx = len(my_list):
+    elif idx > len(my_list) or idx = len(my_list):
         return None
+    else:
+        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
