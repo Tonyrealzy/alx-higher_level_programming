aabd6d22d7a294eaa0cf52dacb2ce7fbe3336d6e
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-19 05:00:29 +0100

    Lists, Tuples tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
-        str.remove(c)
-        return my_string
+
+    new_string = ""
+    for i in my_string:
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string
