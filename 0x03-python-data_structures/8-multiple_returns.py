f64994a488af3eaaea1fc110a1566923641bf34e
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-20 23:26:25 +0100

    lists andtuples tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,8 @@ def multiple_returns(sentence):
     x = len(sentence)
     y = sentence[0]
 
-    if sentence:
-        return (x, y)
+    if not sentence:
+        return (x, None)
 
     else:
-        return (x, None)
+        return (x, y)
