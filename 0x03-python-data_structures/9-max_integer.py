e244e4f3c58d91d669a841e1b645d3668fdd239a
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-20 23:40:00 +0100

    lists andtuples tasks

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) = 0:
+        return None
+    else:
+        my_list = sorted(my_list)
+        x = len(my_list) - 1
+        return my_list[x]
