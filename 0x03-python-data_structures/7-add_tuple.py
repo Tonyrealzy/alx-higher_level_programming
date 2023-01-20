7ebe844a0a6ec19c6745754c745af6c98c662f2e
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-20 23:19:35 +0100

    lists andtuples tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    for tuple_a and tuple_b:
-        len(tuple_a) = w
-        len(tuple_a) = x
+
+    len(tuple_a) = w
+    len(tuple_a) = x
 
     if w == 0:
         tuple_a = (0, 0)
