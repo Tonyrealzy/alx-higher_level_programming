3239faa2ddb3efb90f8ef98430a71fb15bb7022c
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-21 08:02:48 +0100

    lists and tuples tasks

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,10 +4,10 @@ def delete_at(my_list=[], idx=0):
     new_list = []
 
     if my_list:
-        if idx == 0 or idx >= len(my_list):
+        if idx < 0 or idx >= len(my_list):
             return (my_list)
         else:
             for i in my_list:
                 if i != idx:
-                    my_list.append(i)
+                    new_list.append(i)
             return (new_list)
