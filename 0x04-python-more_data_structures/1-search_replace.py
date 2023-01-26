a73d1741908adcee25a163e6ee525633ed6822fc
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-26 06:24:41 +0100

    Set, Dictionary tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,7 +5,7 @@ def search_replace(my_list, search, replace):
 
     for x in my_list:
         if x == search:
-            search = replace
+            x = replace
         new_list.append(x)
 
     return new_list
