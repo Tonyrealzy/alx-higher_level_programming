07a27708beafcae04e8f10882e8b82ae863f6e53
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-27 00:06:27 +0100

    more data structure tasks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary.keys = k
-    a_dictionary.values = v
+    a_dictionary.keys() = k
+    a_dictionary.values() = v
 
     if a_dictionary:
-        print(a_dictionary.keys.sort())
+        print(k.sort(), v)
