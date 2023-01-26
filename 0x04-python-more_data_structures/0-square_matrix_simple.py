fb008ee1f9fa6176c4ac23c2eb38cf3dc83433b9
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-26 06:08:01 +0100

    Set, Dictionary tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     if matrix:
-        square_matrix = list(map((lambda x: x ** 2), y) for y in matrix)
+        square_matrix = list(map((lambda x: x ** 2), y)) for y in matrix
         return square_matrix
