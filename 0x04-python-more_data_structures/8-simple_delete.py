7744ece7184900a1e7a59cf2e864e0d2ce654166
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-27 01:27:50 +0100

    more data structure tasks

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,7 @@
 
 def simple_delete(a_dictionary, key=""):
     for key in a_dictionary:
-        del a_dictionary[key]
+        if key:
+            del a_dictionary[key]
 
     return a_dictionary
