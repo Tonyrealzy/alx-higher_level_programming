8a90bf9be1afb4fd88a9206078c413d9e68d4f5f
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-28 07:07:36 +0100

    more data structure tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,13 +7,13 @@ def roman_to_int(roman_string):
         return (0)
     else:
         roman_to_int = {
-                "I" : 1,
-                "V" : 5,
-                "X" : 10,
-                "L" : 50,
-                "C" : 100,
-                "D" : 500,
-                "M" : 1000
+                "I": 1,
+                "V": 5,
+                "X": 10,
+                "L": 50,
+                "C": 100,
+                "D": 500,
+                "M": 1000
                 }
         result = 0
         s = roman_string
@@ -22,5 +22,5 @@ def roman_to_int(roman_string):
             if i > 0 and roman_to_int[s[i]] > roman_to_int[s[i - 1]]:
                 result += roman_to_int[s[i]] - roman_to_int[s[i - 1]]
             else:
-                result == roman_to_int[s[i]]
+                result += roman_to_int[s[i]]
         return result
