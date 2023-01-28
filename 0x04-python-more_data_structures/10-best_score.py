a204fc015a7a32fd172a69606b81bd7aea571f8c
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-28 06:06:33 +0100

    more data structure tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    if a_dictionary == {}:
+        return None
+    else:
+        for k in a_dictionary:
+            if max(a_dictionary[k]):
+                return k
