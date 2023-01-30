afb13b92c4675e7a3caa88d8071a5856c8451fd2
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-30 22:11:52 +0100

    Python exception tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
                print("{:d}".format(i), end="")
                count += 1
            except Exception:
-               print()
-               return count
+               pass
+        print()
+        return count
 
     
