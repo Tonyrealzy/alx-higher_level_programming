2437b99972c443ca5914157d8137f6cef5d7d752
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-30 21:38:52 +0100

    Python - Exceptions tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     result = isinstance(value, int)
     try:
         if result == True:
-            print("{:d}".format(result))
+            print("{:d}".format(value))
             return True
     except:
         if result == False:
