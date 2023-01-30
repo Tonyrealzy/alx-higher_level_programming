48a549c3c40570066d2afb553d6c1bf4b8ebda47
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-30 22:43:47 +0100

    Python exception tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        if a and b:
+            c = a / b
+    except Exception:
+        return None
+    Inside result:
+        return (c)
+    finally:
+        print("{}".format(c))
