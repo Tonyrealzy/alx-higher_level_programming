205d602b9a93f7ccb5d27dc0d54d2bb725588591
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-31 00:33:50 +0100

    Python exception tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exception: {}".format(value))
+        return False
