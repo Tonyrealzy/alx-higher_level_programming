8f3a350c996db456acf002fcddd886f2154eaa7c
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-30 21:16:17 +0100

    Python - Exceptions tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,14 +3,12 @@
 def safe_print_list(my_list=[], x=0):
 
     count = 0
-    for i in my_list[0:x]:
-        try:
-            print("{}".format(my_list(i)), end="")
+    try:
+        for i in my_list[0:x]:
+            print("{}".format(i), end="")
             count += 1
 
-        except Exception:
-            break
+    except Exception:
+        break
     print()
     return count
-
-
