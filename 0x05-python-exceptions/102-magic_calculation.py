503f2d3656cc0718c2775c7cc925cf963bde807a
Tonyrealzy <umehobiarinze2@gmail.com>
2023-01-31 10:30:16 +0100

    Python exception tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += a ** b / i
-        except Exceptions:
+        except ZeroDivisionError:
             result = b + a
             break
     return result
