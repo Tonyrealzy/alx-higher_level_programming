69bfc08e195f30f03e9d712bd403eaaa2ddcb9d6
Tonyrealzy <umehobiarinze2@gmail.com>
2023-02-07 11:05:36 +0100

    Python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,15 +4,13 @@
 
 class Square:
     """A non-empty class named Square"""
-    
     def __init__(self, size=0):
         self.__size = size
 
-    @property    
+    @property
     def size(self):
         return self.__size
     """Initialising the private method size"""
-    
     @size.setter
     def size(self, value):
         self.__size = value
@@ -21,6 +19,6 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        
     def area(self):
         return (self.__size ** 2)
+        
