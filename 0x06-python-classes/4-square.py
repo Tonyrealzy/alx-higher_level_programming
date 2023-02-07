d66d550cb835fb0c7cc7b090db4bfc17fa29f2c2
Tonyrealzy <umehobiarinze2@gmail.com>
2023-02-07 10:56:03 +0100

    Python classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module defines the class Square"""
+
+
+class Square:
+    """A non-empty class named Square"""
+    
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property    
+    def size(self):
+        return self.__size
+    """Initialising the private method size"""
+    
+    @size.setter
+    def size(self, value):
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+        
+    def area(self):
+        return (self.__size ** 2)
