5632fc29a52549d61b3ded91b57297e9bfe4aa61
Tonyrealzy <umehobiarinze2@gmail.com>
2023-02-07 04:04:40 +0100

    Python Classes tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""Module is as defined previously"""
+
+class Square:
+    """Size is initialised as a private variable"""
+    def __init__(self, size):
+        self.size = __size
