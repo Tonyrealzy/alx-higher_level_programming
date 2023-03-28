ae876baacfb3c8475aa42940e15b314aeee1c362
Tonyrealzy <umehobiarinze2@gmail.com>
2023-03-28 15:18:53 +0100

    New commits

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,6 +7,10 @@ IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE 
 IF NOT EXISTS `hbtn_0d_2`;
 
+GRANT USAGE
+ON *.*
+TO 'user_0d_2'@'localhost';
+
 GRANT SELECT
 0N `hbtn_0d_2`.*
 TO 'user_0d_2'@'localhost';
