acecc9824c39641328c81d4fa7d5e6482b8ff992
Tonyrealzy <umehobiarinze2@gmail.com>
2023-04-18 14:17:47 +0100

    Javascript warm-up tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 //script that prints 3 lines but by using an array of string and a loop
 
-let myArray = {"C is fun", "Python is cool", "JavaScript is amazing"};
+const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < 3; i++) {
     console.log(myArray[i]);
