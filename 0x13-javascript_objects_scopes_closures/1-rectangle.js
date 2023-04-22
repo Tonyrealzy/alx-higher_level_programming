a6e29a9dba98ad551e4a455dd0be49de8a426ae5
Tonyrealzy <umehobiarinze2@gmail.com>
2023-04-22 06:07:10 +0100

    Javascript object tasks

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,9 +3,9 @@
 //class Rectangle that defines a rectangle
 
 class Rectangle {
-    constructor(width, height){
-        this.width = 'w';
-        this.height = 'h';
+    constructor(w, h){
+        this.width = w;
+        this.height = h;
     }
 }
 
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,7 +3,7 @@
 //class Rectangle that defines a rectangle
 
 class Rectangle {
-    constructor(width, height){
+    constructor(w, h){
         if ((w > 0) && (h > 0)) {
         this.width = w;
         this.height = h;
