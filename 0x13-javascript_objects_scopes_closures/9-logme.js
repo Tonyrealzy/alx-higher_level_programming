1500ed4f048ae067a74d1309f34e1f4cec1c25a1
Tonyrealzy <umehobiarinze2@gmail.com>
2023-04-22 12:08:47 +0100

    JavaScript - Objects, Scopes and Closures tasks

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -3,7 +3,7 @@
 //function that prints the number of arguments already printed and the new argument value
 
 exports.logMe = function (item) {
-    for (let i = 2; i < arguments.length; i++) {
-        console.log('i: ' + arguments[i]);
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(i + ' ' + process.argv[i + 2]);
     }
 }
