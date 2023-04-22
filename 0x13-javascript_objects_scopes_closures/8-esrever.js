d1df8299f8193574d9640721be65bdccf29477ee
Tonyrealzy <umehobiarinze2@gmail.com>
2023-04-22 07:20:49 +0100

    JavaScript - Objects, Scopes and Closures tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,5 +3,7 @@
 //function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-    
+    for (let i = 0; i < list.length; i++) {
+        list[i] = list[list.length - 1]
+    }
 }
