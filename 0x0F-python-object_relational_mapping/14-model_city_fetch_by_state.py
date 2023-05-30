157dd1546505fc1a497efc62ef4b12b2dd9960ab
Tonyrealzy <umehobiarinze2@gmail.com>
2023-05-30 13:35:15 +0100

    Python ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,8 +3,8 @@
 from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
-
     import sys
+    
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
