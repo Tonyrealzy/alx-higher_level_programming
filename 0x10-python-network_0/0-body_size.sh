39bf0dfe965cb0998d814324e5c652f707cf0d64
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-18 20:09:41 +0100

    0x10-python-network_0 tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that akes in a URL
-#sends a request and displays size of response body
+# Bash script that akes in a URL that sends a request and displays size of response body
 curl -s "$1" | wc -c
